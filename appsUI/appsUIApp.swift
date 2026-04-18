7cf2693719a5adba10102eddcf0bb7d06ad4ade6
chefmarvinimana-cyber <chefmarvinimana@gmail.com>
2026-04-18 07:12:12 +0400

    Initial Commit

---
+++ appsUI/appsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  appsUIApp.swift
+//  appsUI
+//
+//  Created by Mar Vin on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct appsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
